a9cc557a366b101785bd9156faccc039b3939c63
michael.li <michael.li@thoughtworks.com>
2024-01-19 14:53:47 +0800

    uncomment the M4 instances

--- Exercise_1/main.tf
+++ Exercise_1/main.tf
@@ -18,12 +18,12 @@ resource "aws_instance" "Udacity_T2" {
 
 
 # TODO: provision 2 m4.large EC2 instances named Udacity M4
-# resource "aws_instance" "Udacity_M4" {
-#   count = "2"
-#   ami = "ami-0323c3dd2da7fb37d"
-#   subnet_id = "subnet-0f610509657c97591"
-#   instance_type = "m4.large"
-#   tags = {
-#     Name = "Udacity M4"
-#   }
-# }
+resource "aws_instance" "Udacity_M4" {
+  count = "2"
+  ami = "ami-0323c3dd2da7fb37d"
+  subnet_id = "subnet-0f610509657c97591"
+  instance_type = "m4.large"
+  tags = {
+    Name = "Udacity M4"
+  }
+}
